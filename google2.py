f0649a9890fac09eda2a3ce2cad07f62c2e084bf
Artem Kravchenko <artem.kravchenko@exness.com>
2023-11-06 23:37:35 +0300

    fix google2 testcase

--- google2.py
+++ google2.py
@@ -1,18 +1,22 @@
 def solution(xs):
+  if len(xs) == 1:
+    return str(xs[0])
   sol = 1
   counter = 0
   tmp = float("-inf")
   for i in xs:
-    if i > 0:
-      sol *= i
-      counter += 1
-    if i < 0:
-      if i > tmp:
-          tmp = i
-      sol *= i
-      counter += 1
+      if i > 0:
+          sol *= i
+          counter += 1
+      if i < 0:
+          if i > tmp:
+              tmp = i
+          sol *= i
+          counter += 1
   if sol < 0 and counter != 1:
-    sol = sol // tmp
+      sol = sol // tmp
   if counter == 0 or (sol < 0 and counter == 1):
-    sol = 0
+      sol = 0
   return str(sol)
+
+print(solution([-6]))
